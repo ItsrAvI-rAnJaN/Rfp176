d20a48332d8f0bbc4f5247b7bf5e4d2aec625825
ravi04ez <ezekie.em@gmail.com>
2022-07-06 00:33:22 +0530

    initial commit

---
+++ HelloGreetings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloGreetings
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everybody, Greeting from Ravi Ranjan");
+            Console.ReadKey();
+        }
+        
+    }
+}
